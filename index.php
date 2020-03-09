8fddaa85d868eda8040266c9328dc82ac80e5870
Andres Morales <mad08061@gmail.com>
2020-03-09 14:00:31 -0500

    inicio/fin proyecto BSD

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <form>
+    <table border="1" cellspacing="0" cellpadding="3" >
+        <tr style="background-color: violet">
+            <td>id</td>
+            <td>nombre</td>
+            <td>email</td>
+        </tr>
+        <tr >
+            <?php
+            $con= new mysqli("localhost","root","","personas");
+            $query="select * from personas";
+            $result=$con->query($query);//--> mysqli_query($con,$query)
+            while($mostrar=mysqli_fetch_array($result)){
+            ?>
+            <td><?php echo $mostrar['id']?></td>
+            <td><?php echo $mostrar['nombre']?></td>
+            <td><?php echo $mostrar['email']?></td>
+        </tr>
+            <?php 
+            }
+            $con->close();
+            ?>
+    </table>
+    </form>
+    <form action="crear.php">
+    <div>
+        <br>
+        <button type="submit">Agregar</button>
+    </div>
+    </form>
+</body>
+</html>
