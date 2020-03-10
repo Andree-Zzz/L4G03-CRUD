06df84e3c97d5e8db34080ea3cb1b062f3d83dbb
Andres Morales <mad08061@gmail.com>
2020-03-09 19:06:36 -0500

    actualizacion, base con datos

--- personas.sql
+++ personas.sql
@@ -11,7 +11,29 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura de base de datos para personas
+CREATE DATABASE IF NOT EXISTS `personas` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `personas`;
+
+-- Volcando estructura para tabla personas.personas
+CREATE TABLE IF NOT EXISTS `personas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(80) DEFAULT NULL,
+  `email` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla personas.personas: ~5 rows (aproximadamente)
+/*!40000 ALTER TABLE `personas` DISABLE KEYS */;
+INSERT INTO `personas` (`id`, `nombre`, `email`) VALUES
+	(1, 'andres', 'mad08061@gmail.com'),
+	(2, 'frederick', 'perro@gg'),
+	(3, 'artur', 'ssj@gmail.com'),
+	(4, '', ''),
+	(5, 'andres', 'mad08061@gmail.com'),
+	(6, 'camo', '230o120o21');
+/*!40000 ALTER TABLE `personas` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
